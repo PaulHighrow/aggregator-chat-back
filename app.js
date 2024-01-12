5fb2e906f3461326b1534af1aa83641f4435f300
Kyrylo Sulihan <106202828+PaulHighrow@users.noreply.github.com>
2024-01-12 18:08:31 +0200

    deletion of unused

--- app.js
+++ app.js
@@ -40,7 +40,6 @@ io.on("connection", (socket) => {
   socket.on("message", (data) => {
     console.log(data);
     socket.emit("message", data);
-    router.post("messages", validateMessage, addMessage);
   });
 
   socket.on("disconnect", () => {
