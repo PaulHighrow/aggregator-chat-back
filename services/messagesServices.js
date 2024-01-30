94a3d63ee8ca494cf1c400d942b9cab1939ae310
Kyrylo Sulihan <paulhighrow@gmail.com>
2024-01-30 15:36:59 +0200

    pin fix

--- services/messagesServices.js
+++ services/messagesServices.js
@@ -5,7 +5,7 @@ const getAllMessages = async () => await Messages.find({});
 const newMessage = async (body) => await Messages(body).save();
 
 const updateMessage = async (id, update) =>
-  await Messages.findByIdAndUpdate(id, update, { new: true });
+  await Messages.findOneAndUpdate(id, update, { new: true });
 
 const deleteMessage = async (id) => await Messages.findOneAndDelete(id);
 
